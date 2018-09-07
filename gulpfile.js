005b4813b24562e367b946866ad565e26c73564f
Manish Menaria <the.manish.menaria@gmail.com>
2018-09-07 12:10:44 +0530

    basic gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp')
+
+// Styles
+gulp.task('styles', function(){
+    console.log('Starting styles tasks');
+});
+
+// Scripts
+gulp.task('scripts', function(){
+    console.log('Starting scripts Task');
+});
+
+// Images
+gulp.task('images', function(){
+    console.log('Starting Images Task');
+});
+
+// Default
+gulp.task('default', function(){
+    console.log('Starting Default Task')
+});
