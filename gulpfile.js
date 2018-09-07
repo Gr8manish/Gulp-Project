340a217da39dd36c10223ef3d5f863d6fd73f414
Manish Menaria <the.manish.menaria@gmail.com>
2018-09-07 13:03:00 +0530

    CSS concatination

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
-var gulp = require('gulp')
-var uglify = require('gulp-uglify')
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 // File Paths
+var DIST_PATH = 'public/dist/'
 var SCRIPTS_PATH = 'public/scripts/**/*.js'
+var CSS_PATH = 'public/css/**/*.css'
+
 
 // Styles
 gulp.task('styles', function(){
     console.log('Starting styles tasks');
+
+    return gulp.src(['public/css/reset.css',CSS_PATH])
+            .pipe(concat('styles.css'))
+            .pipe(gulp.dest(DIST_PATH));
 });
 
 // Scripts
